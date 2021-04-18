7f62500c76c46c02da1b987ec0a5c3397ffa587e
mariiaalok <mariia.alokhina@qpard.com>
2021-04-18 18:23:01 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "alokhina"
+
+version := "0.1"
+
+scalaVersion := "2.13.5"
+
+idePackagePrefix := Some("alokhina.lab1")
---
+++ src/main/scala/HelloWorldApp.scala
@@ -0,0 +1,13 @@
+package alokhina.lab1
+
+import scala.io.StdIn
+
+object task1 extends App{
+  val name = StdIn.readLine("Hi! Enter your name:")
+  println(s"11111 $name!")
+}
+
+object task2 extends App{
+  val name = StdIn.readLine("Hi! Enter your name:")
+  println(s"3333 $name!")
+}
